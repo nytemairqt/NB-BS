8a21768939310511759796dff5ca83612937aa64
Cody <nytemairqt@gmail.com>
2024-10-03 00:48:41 +1000

    Added note filtering for residue samples

--- Scripts/ScriptProcessors/NB-BS/leftRS_midiProcessor.js
+++ Scripts/ScriptProcessors/NB-BS/leftRS_midiProcessor.js
@@ -3,7 +3,10 @@ reg m = 0;
 reg v = 0;function onNoteOn()
 {
 	m = Message.getNoteNumber();
-	v = Message.getVelocity();		
+	v = Message.getVelocity();	
+	
+	if (m > 64)	
+		Message.ignoreEvent(true);
 }
  function onNoteOff()
 {
